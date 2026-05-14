215b224623e93724d87e3f9dd8fb0e6c5dd6c876
moulsssss <94977037+vmoulinet@users.noreply.github.com>
2026-05-15 02:52:42 +0900

    fmod cleaning

--- Assets/Samples/Scriptable Render Pipeline Core/17.3.0/Common/Scripts/InstallPackage.cs
+++ Assets/Samples/Scriptable Render Pipeline Core/17.3.0/Common/Scripts/InstallPackage.cs
@@ -1,11 +1,12 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
+#endif
 using UnityEngine;
 
 public class InstallPackage : MonoBehaviour
 {
     public string packageName = "com.unity.inputsystem";
-    
-}
 
+}
